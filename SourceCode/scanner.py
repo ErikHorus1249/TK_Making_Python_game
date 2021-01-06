10b191c0a9a376bef7712ff36e6e389908340c26
Erik_Horus <tuananh1421999@gmail.com>
2021-01-06 19:12:54 +0700

    up

--- SourceCode/scanner.py
+++ SourceCode/scanner.py
@@ -8,15 +8,15 @@ data['block'] = []   # du lieu dang list
 #dem, count = 0,0
 	
 def scan(pos,HEIGHT,WIDTHX,WIDTHZ):
-	file = open('data.json', "w") # w - write 
-	WIDTHX = WIDTHX/2
-	WIDTHZ = WIDTHZ/2
-	for  i in range(int(pos.x-WIDTHX),int(pos.x+WIDTHX)):
+	file = open('tam022.json', "w") # w - write 
+	WIDTHX = WIDTHX/2 + 1
+	WIDTHZ = WIDTHZ/2 + 1
+	for  i in range(int(pos.x-WIDTHX-1),int(pos.x+WIDTHX)):
 		mc.postToChat("Scanning...") # in ra thong diep dang quet
-   		for  j in range(int(pos.y-1),int(pos.y+HEIGHT)):
-       			for  k in range(int(pos.z-WIDTHZ),int(pos.z+WIDTHZ)):
+   		for  j in range(int(pos.y-2),int(pos.y+HEIGHT)):
+       			for  k in range(int(pos.z-WIDTHZ-1),int(pos.z+WIDTHZ)):
            			bId = mc.getBlock(i,j,k)
-           			if bId :
+           			if bId >= 0 :
 						data['block'].append({
 						'x' : i-pos.x,
 						'y' : j-pos.y,
@@ -26,11 +26,11 @@ def scan(pos,HEIGHT,WIDTHX,WIDTHZ):
    	
 	json.dump(data,file)
     
-	# with open('BlockData2.json','w') as file:
+	#with open('BlockData2.json','w') as file:
     # 	json.dump(data,file)    
 	
 pos = mc.player.getPos();
-scan(pos,20,50,50)
+scan(pos,20,25,25)
 
 
 mc.postToChat("DONE") 
